735349f53105f89e1d7283869223aba3ea1be1a1
Stephen <StephenLewis115@gmail.com>
2017-12-07 15:27:46 -0600

    Added red text for a countdown under 0, and a sound at 0

--- Countdown-Clock/app/js/countdown.js
+++ Countdown-Clock/app/js/countdown.js
@@ -3,9 +3,16 @@ var datePicker = $("input[name='datePicker']");
 datePicker.val("2017-12-08T14:00");
 var countDownDate = new Date(datePicker.val()).getTime();
 
+var sound = new Howl({
+	src: ['./app/assets/party_horn-Mike_Koenig-76599891.wav']
+});
+
+
+
 console.log(countDownDate)
 clock();
 timer();
+
 
 
 function clock() {
@@ -16,17 +23,29 @@ function clock() {
 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 	h2.html(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
+	if(distance < 1000) {
+		h2.addClass("finished");
+	}
+
+	if(distance < 1000 && distance > -1000) {
+		sound.play();
+	}
 }
 
+
+
 function timer() {
-	setInterval(function(){
+	setInterval(function() {
 		clock();
 	}, 1000)
 }
 
-datePicker.change(function(){
+
+
+datePicker.change(function() {
 	console.log(datePicker.val());
+	h2.removeClass("finished");
 	countDownDate = new Date(datePicker.val()).getTime();
-clock();
+	clock();
 
 });
